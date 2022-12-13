1a4606d47bf3815dbf50ab9512099ffe6f625924
sayadkhan <abdullahalsayad03@gmail.coml>
2022-12-13 20:41:31 +0600

    server ready

---
+++ index.js
@@ -0,0 +1,9 @@
+const jasonServer = require("json-server");
+const server = jasonServer.create();
+const router = jasonServer.router("db.json");
+const middlewares = jasonServer.defaults();
+const port = process.env.port || 5000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
